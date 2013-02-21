9d9a31a391f534da1977ab166f5d69ae637875c5
Elijah Insua <tmpvar@gmail.com>
2013-02-20 23:42:31 -0700

    add -i which toggles to focused view

---
+++ README.md
@@ -0,0 +1,23 @@
+# gcode-simulator
+
+Webgl based gcode simulator
+
+## Uses
+
+### from the command line
+
+First, install `npm install -g gcode-simulator`
+
+now you can execute the `gcode-simulator` command at your prompt.  If you want to stream gcode to the simulator add a `-i`.
+
+_example_: `curl http://www.clker.com/cliparts/O/H/c/W/A/o/plane.svg | node bin/svgmill.js | gcode-simulator`
+
+where `svgmill.js` turns svg into gcode
+
+### from the web
+
+open index.html in a webgl capable browser
+
+## License
+
+MIT
--- bin/gcode-simulator.js
+++ bin/gcode-simulator.js
@@ -1,39 +1,13 @@
 #!/usr/bin/env node
 
 var nc = require('node-chrome');
-
-// switch (process.platform) {
-//   case 'darwin':
-//     if (appName) {
-//       opener = 'open -a "' + escape(appName) + '"';
-//     } else {
-//       opener = 'open';
-//     }
-//     break;
-//   case 'win32':
-//     // if the first parameter to start is quoted, it uses that as the title
-//     // so we pass a blank title so we can quote the file we are opening
-//     if (appName) {
-//       opener = 'start "" "' + escape(appName) + '"';
-//     } else {
-//       opener = 'start ""';
-//     }
-//     break;
-//   default:
-//     if (appName) {
-//       opener = escape(appName);
-//     } else {
-//       // use Portlands xdg-open everywhere else
-//       opener = path.join(__dirname, '../vendor/xdg-open');
-//     }
-//     break;
-// }
+var argv = require('optimist').argv;
 
 nc({
   runtime: "/Volumes/Data/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome",
   port: 8080,
   files: __dirname + '/../',
-  index: "/bare-index.html",
+  index: (argv.i) ? "/bare-index.html" : "/index.html",
   width: 1024,
   height: 768
 }, function(ws, chrome) {
@@ -42,21 +16,17 @@ nc({
     gcode+=d;
   });
 
-  process.stdin.on('end', function() {
-    ws.send(gcode);
-  });
+  if (argv.i) {
+    process.stdin.on('end', function() {
+      ws.send(gcode);
+    });
 
-  ws.on('data', function(d) {
-    if (d.toString().indexOf('done') > -1) {
-      process.exit();
-    }
-  });
+    ws.on('data', function(d) {
+      if (d.toString().indexOf('done') > -1) {
+        process.exit();
+      }
+    });
 
-  ws.on('close', function() {
-    chrome.kill('SIGQUIT');
-    process.exit();
-  });
-
-
-  process.stdin.resume();
+    process.stdin.resume();
+  }
 });
